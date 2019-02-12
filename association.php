d697efa12843e226911dac28cf3dab84b1999a8b
sadayama <sada020915058@gmail.com>
2019-02-12 17:44:35 +0900

    Update association.php

---
+++ association.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>テスト</title>
+  <style type="text/css">
+
+  </style>
+</head>
+
+<body>
+<h1 id="btn">クラス</h1>
+<?php
+//連想配列
+$association = array(
+    // キー => 値
+    'test' => 'テスト',
+    'sample' => 'サンプル'
+);
+echo $association['test'] ;
+echo '<br>';
+// 追加は変数名['キー'] = '値';
+$association['addition'] = '追加されました';
+echo $association['addition'];
+
+// foreach文
+// 配列または連想配列に対して、先頭のデータから順に繰り返し処理を行う
+// 配列
+$sample = array('配列','出ました');
+foreach($sample as $value){
+    echo '<br>';
+    echo $value;
+}
+
+// 連想配列
+foreach($association as $value){
+    echo '<br>';
+    echo $value;
+}
+?>
+<script type="text/javascript">
+  "use strict";
+const btn = document.getElementById("btn")
+window.addEventListener('load',()=> {
+
+});
+</script>
+
+</html>
