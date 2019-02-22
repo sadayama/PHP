619abe2aaf1770ea539afd75e2b61ef0da8239bd
sadayama <44350576+sadayama@users.noreply.github.com>
2019-02-22 13:18:42 +0900

    Add files via upload

---
+++ return.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>テスト</title>
+  <style type="text/css">
+
+  </style>
+</head>
+
+<body>
+<h1 id="btn">クラス</h1>
+<?php
+    function sample(){
+        // return(値)　で関数の戻り値を返す
+        return "戻り値";
+    };
+    // sample()の戻り値はreturnで返された "戻り値"
+    $test =sample();
+    echo $test;
+?>
+<script type="text/javascript">
+  "use strict";
+const btn = document.getElementById("btn")
+window.addEventListener('load',()=> {
+
+});
+</script>
+
+</html>
