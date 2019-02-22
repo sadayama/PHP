12dd4c7a295856e5edfd3afa4afb42fb73fc4946
sadayama <44350576+sadayama@users.noreply.github.com>
2019-02-22 12:55:11 +0900

    Add files via upload

---
+++ rand.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>テスト</title>
+  <style type="text/css">
+
+  </style>
+</head>
+
+<body>
+<h1 id="btn">クラス</h1>
+<?php
+// rand(引数1,引数2)
+// 引数1から引数2までの間でランダムの値を返す
+echo rand(1,60);
+?>
+<script type="text/javascript">
+  "use strict";
+const btn = document.getElementById("btn")
+window.addEventListener('load',()=> {
+
+});
+</script>
+
+</html>
