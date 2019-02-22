c59eec04c7e2009d605dcca7cb2fd1cf8042285a
sadayama <44350576+sadayama@users.noreply.github.com>
2019-02-22 12:53:32 +0900

    Add files via upload

---
+++ count.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>テスト</title>
+  <style type="text/css">
+
+  </style>
+</head>
+
+<body>
+<h1 id="btn">クラス</h1>
+<?php
+$sample = array("サンプル1","サンプル2","サンプル3");
+// countは配列の要素数を返す
+echo count($sample);
+?>
+<script type="text/javascript">
+  "use strict";
+const btn = document.getElementById("btn")
+window.addEventListener('load',()=> {
+
+});
+</script>
+
+</html>
