f896d477a5c95bc2ea1db04dd0cf7f9bdf999d3e
sadayama <44350576+sadayama@users.noreply.github.com>
2019-02-22 12:06:18 +0900

    Add files via upload

---
+++ for.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>テスト</title>
+  <style type="text/css">
+
+  </style>
+</head>
+
+<body>
+<h1 id="btn">クラス</h1>
+<?php
+
+?>
+<script type="text/javascript">
+  "use strict";
+const btn = document.getElementById("btn")
+window.addEventListener('load',()=> {
+
+});
+</script>
+
+</html>
