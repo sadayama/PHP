6d99bac68cd7af1f5c5f273242995647f8e11c56
sadayama <44350576+sadayama@users.noreply.github.com>
2019-02-22 12:47:24 +0900

    Add files via upload

---
+++ strlen.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>テスト</title>
+  <style type="text/css">
+
+  </style>
+</head>
+
+<body>
+<h1 id="btn">クラス</h1>
+<?php
+$chr = "文字列";
+// 文字列の文字数を返す関数
+// strlen(文字列)
+// ()内には変数を入れても良い
+echo strlen($chr); //9
+
+$chr = "test";
+echo strlen($chr); //4
+?>
+<script type="text/javascript">
+  "use strict";
+const btn = document.getElementById("btn")
+window.addEventListener('load',()=> {
+
+});
+</script>
+
+</html>
